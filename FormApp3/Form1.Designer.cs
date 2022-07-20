e9bfd8d5d7df5f52c9e59c5fd9e50b4df802325d
desrosiersm <desrosiersm@DESROSIERSM1022>
2022-07-20 09:32:13 -0400

    Add project files.

---
+++ FormApp3/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FormApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.StripChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_ConnectedIdentifier = new System.Windows.Forms.Label();
+            this.label_ConnectionDisplay = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox_Dates = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.StripChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StripChart
+            // 
+            this.StripChart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea2.Name = "ChartArea1";
+            this.StripChart.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.StripChart.Legends.Add(legend2);
+            this.StripChart.Location = new System.Drawing.Point(50, 50);
+            this.StripChart.Margin = new System.Windows.Forms.Padding(0);
+            this.StripChart.Name = "StripChart";
+            series3.BorderWidth = 5;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "FilterData";
+            series4.BorderWidth = 5;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "TempData";
+            this.StripChart.Series.Add(series3);
+            this.StripChart.Series.Add(series4);
+            this.StripChart.Size = new System.Drawing.Size(716, 389);
+            this.StripChart.TabIndex = 0;
+            this.StripChart.Text = "chart1";
+            this.StripChart.Click += new System.EventHandler(this.StripChart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_ConnectedIdentifier
+            // 
+            this.label_ConnectedIdentifier.AutoSize = true;
+            this.label_ConnectedIdentifier.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ConnectedIdentifier.Location = new System.Drawing.Point(50, 9);
+            this.label_ConnectedIdentifier.Name = "label_ConnectedIdentifier";
+            this.label_ConnectedIdentifier.Size = new System.Drawing.Size(192, 26);
+            this.label_ConnectedIdentifier.TabIndex = 1;
+            this.label_ConnectedIdentifier.Text = "Connection status:";
+            this.label_ConnectedIdentifier.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_ConnectionDisplay
+            // 
+            this.label_ConnectionDisplay.AutoSize = true;
+            this.label_ConnectionDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_ConnectionDisplay.Location = new System.Drawing.Point(248, 9);
+            this.label_ConnectionDisplay.Name = "label_ConnectionDisplay";
+            this.label_ConnectionDisplay.Size = new System.Drawing.Size(156, 26);
+            this.label_ConnectionDisplay.TabIndex = 2;
+            this.label_ConnectionDisplay.Text = "Disconnected";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // comboBox_Dates
+            // 
+            this.comboBox_Dates.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Dates.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_Dates.FormattingEnabled = true;
+            this.comboBox_Dates.Location = new System.Drawing.Point(490, 6);
+            this.comboBox_Dates.Name = "comboBox_Dates";
+            this.comboBox_Dates.Size = new System.Drawing.Size(207, 33);
+            this.comboBox_Dates.TabIndex = 3;
+            this.comboBox_Dates.SelectedIndexChanged += new System.EventHandler(this.comboBox_Dates_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox_Dates);
+            this.Controls.Add(this.label_ConnectionDisplay);
+            this.Controls.Add(this.label_ConnectedIdentifier);
+            this.Controls.Add(this.StripChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.StripChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_ConnectedIdentifier;
+        private System.Windows.Forms.Label label_ConnectionDisplay;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ComboBox comboBox_Dates;
+        private System.Windows.Forms.DataVisualization.Charting.Chart StripChart;
+    }
+}
+
---
+++ FormApp3/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments;
+using NationalInstruments.DAQmx;
+using NWaves.Signals;
+using NWaves.Filters;
+using Task = NationalInstruments.DAQmx.Task;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace FormApp3
+{
+    public partial class Form1 : Form
+    {
+        AnalogSingleChannelReader reader;
+        AIChannel temperature;
+        bool connectedStatus = false;
+        long thermocoupleID = 31500308;
+        SqlConnection connection;
+        String connectionString = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+        List<DateTime> x = new List<DateTime>();
+        List<float> y = new List<float>();
+        int selectedIdx = -1;
+
+        private void attachToDevice()
+        {
+            String[] devices = DaqSystem.Local.Devices;
+            if (devices.Length == 0) return;
+            string thermocoupleDeviceId = null;
+            for (int i = 0; i < devices.Length; i++)
+            {
+                Device thisDevice = DaqSystem.Local.LoadDevice(devices[i]);
+                string dev_id = thisDevice.DeviceID;
+                long serial_number = thisDevice.SerialNumber;
+                if (serial_number == thermocoupleID)
+                {
+                    thermocoupleDeviceId = dev_id;
+                    break;
+                }
+            }
+            if (thermocoupleDeviceId == null) return;
+            Task tempTask = new Task();
+
+            temperature = tempTask.AIChannels.CreateThermocoupleChannel(
+                    "Dev1/ai0",
+                    "Temperature",
+                    0,
+                    100,
+                    AIThermocoupleType.J,
+                    AITemperatureUnits.DegreesF
+                );
+            tempTask.Stream.Timeout = 1000;
+
+            reader = new AnalogSingleChannelReader(tempTask.Stream);
+            
+            connectedStatus = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String query = "SELECT * from TemperatureLog";
+            SqlCommand command = new SqlCommand(query);
+            command.Connection = connection;
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int x_ = Convert.ToInt32(reader["SampleTime"]);
+                    double t = Convert.ToDouble(reader["Temperature"]); ;
+                    if (x_ > 1000) {
+                        DateTime dt0 = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                        x.Add(dt0.AddSeconds(x_).ToLocalTime());
+                        y.Add(((float)t));
+                    }
+                }
+            }
+        }
+
+        int i = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateConnctionMessage();
+            if (!connectedStatus)
+            {
+                attachToDevice();
+                return;
+            }
+            try {
+                float sample = (float) reader.ReadSingleSample();
+                DateTime now = System.DateTime.Now;
+                x.Add(now);
+                y.Add(sample);
+
+                 // send new value to database
+                String query = "INSERT INTO TemperatureLog (SampleTime,Temperature) VALUES (@SampleTime,@Temperature)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@SampleTime", ((DateTimeOffset)now).ToUnixTimeSeconds());
+                command.Parameters.AddWithValue("@Temperature", sample);
+                int affected = command.ExecuteNonQuery();
+
+
+                StripChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+                StripChart.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+
+
+                IEnumerable<DateTime> selected_x;
+                IEnumerable<float> selected_y;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                if (selectedIdx < 0)
+                {
+                    selected_x = x; // x.ToArray();
+                    selected_y = y; //y.ToArray();                    
+                } else
+                {
+                    var selected_date = dates.ElementAt(selectedIdx);
+                    var indices = from dt in x
+                                  where dt.Date == selected_date
+                                  select x.IndexOf(dt);
+                    //var idxs = indices.Mi
+                    int min_idx = indices.Min();
+                    Console.WriteLine("0) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                    int cnt = indices.Max() - min_idx;
+                    Console.WriteLine("1) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                    var plot_x = x.GetRange(min_idx, cnt);
+                    var plot_y = y.GetRange(min_idx, cnt);
+                    //IEnumerable<DateTime> plot_x = (from idx in indices
+                     //                              select x.ElementAt(idx)).ToList();
+                    Console.WriteLine("2) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                    //IEnumerable<float> plot_y = (from idx in indices
+                     //                           select y.ElementAt(idx)).ToArray();
+                    Console.WriteLine("3) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+
+                    selected_x = plot_x; // plot_x.ToList(); //x.ToArray();
+                    selected_y = plot_y; // plot_y.ToList(); //y.ToArray();
+                    Console.WriteLine("4) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                    
+                    
+
+                }
+                List<DateTime> dtttt = selected_x.ToArray().ToList();
+                int count = 1;
+                Console.WriteLine("Number of points: " + count.ToString()); //  Length.ToString());
+                Console.WriteLine("5) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                if (count > 10000)
+                {
+                    //int modulus = selected_x.Count() / 10000;
+                    //selected_x = selected_x.Where((x, i) => i % modulus == 0).ToArray();
+                    //selected_y = selected_y.Where((x, i) => i % modulus == 0).ToArray();
+                }
+                Console.WriteLine("6) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms");
+                StripChart.Series[0].Points.DataBindXY(selected_x, selected_y);
+                Console.WriteLine("7) Took " + stopWatch.ElapsedMilliseconds.ToString() + " ms\n");
+                stopWatch.Stop();
+                var available_dates = from samp in selected_x
+                            select samp.Date;
+                available_dates = available_dates.Distinct();
+                if (available_dates.Count() > 1)
+                {
+                    StripChart.Series[0].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+                    StripChart.Series[1].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+                }
+                else
+                {
+                    StripChart.Series[0].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+                    StripChart.Series[1].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+                }
+
+                StripChart.ChartAreas[0].AxisY.Minimum = Math.Round(selected_y.Min() - 1);
+                StripChart.ChartAreas[0].AxisY.Maximum = Math.Round(selected_y.AsQueryable().Max() + 1);
+
+                try
+                {
+                    DiscreteSignal ds = new DiscreteSignal(1, y.ToArray());
+                    SavitzkyGolayFilter sgf = new SavitzkyGolayFilter(15, 0);
+                    ds = sgf.ApplyTo(ds);
+                    // StripChart.Series[1].Points.DataBindXY(x, ds.Samples);
+                }
+                catch { }
+                
+                i++;
+            } catch
+            {
+                Console.WriteLine("Disconnected! 😢");
+                connectedStatus = false;
+            }
+            
+        }
+
+        private void updateConnctionMessage()
+        {
+            if (connectedStatus)
+            {
+                label_ConnectionDisplay.Text = "Connected";
+                label_ConnectionDisplay.ForeColor = Color.Green;
+            } else
+            {
+                label_ConnectionDisplay.Text = "Disconnected";
+                label_ConnectionDisplay.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int BUFFER_TOP = 50;
+            int BUFFER_BOT = 100;
+            int BUFFER_LEFT = 50;
+            int BUFFER_RIGHT = 50;
+            Size newSize = Form1.ActiveForm.Size;
+            Console.Write("New form size is: ");  Console.WriteLine(newSize.ToString());
+            newSize.Width = newSize.Width - BUFFER_LEFT - BUFFER_RIGHT;
+            newSize.Height = newSize.Height - BUFFER_TOP - BUFFER_BOT;
+            StripChart.Size = newSize;
+            Console.Write("New plot size is: "); Console.WriteLine(newSize.ToString());
+            // StripChart.Location = new Point(BUFFER_TOP, BUFFER_LEFT);
+            StripChart.Left = BUFFER_LEFT;
+            StripChart.Top = BUFFER_TOP;
+        }
+
+        private void StripChart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        List<DateTime> dates;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var t_dates = from samp in x
+                          select samp.Date.ToString("dd/MM/yy");
+            
+            t_dates = t_dates.Distinct();
+            t_dates = t_dates.OrderBy(name => name);
+
+            t_dates = t_dates.Prepend("All");
+            if ((dates != null) && (t_dates.Count()-1 == dates.Count()))
+            {
+                return;
+            } else
+            {
+                Console.WriteLine(t_dates.Count().ToString() + " / " + (dates == null ? 0 : dates.Count()));
+            }
+            
+            
+            System.Object[] io = new System.Object[t_dates.Count()];
+            for (int i = 0; i < t_dates.Count(); i++)
+            {
+                io[i] = t_dates.ElementAt(i);
+            }
+            comboBox_Dates.Items.Clear();
+            comboBox_Dates.Items.AddRange(io);
+            comboBox_Dates.SelectedIndex = 0;
+
+            var n_dates = from samp in x
+                          select samp.Date;
+            dates = n_dates.Distinct().ToList();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (connection.State != ConnectionState.Closed) {
+                connection.Close();
+            }
+            
+        }
+
+        private void comboBox_Dates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (dates != null)
+            {
+                selectedIdx = comboBox_Dates.SelectedIndex - 1;                
+            }
+        }
+    }
+}
